9c7f026e1dd148d37b23a61dfa375c0096896a71
Percival Garcia <perrgarcia17@gmail.com>
2024-12-13 09:47:25 +0800

    Create useCounter.ts
    add composables files

---
+++ app/composables/useCounter.ts
@@ -0,0 +1,17 @@
+import { ref, computed } from 'vue'
+
+export default function useCounter() {
+  const count = ref(0)
+
+  const increment = () => {
+    count.value++
+  }
+
+  const doubleCount = computed(() => count.value * 2)
+
+  return {
+    count,
+    doubleCount,
+    increment,
+  }
+}
